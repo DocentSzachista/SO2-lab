ea697f02498c92c21d0c6309c3dfc3db5dec9ab0
ubuntu <idontknow5325@gmail.com>
2022-06-12 02:50:39 -0700

    add lab2 and 3 solutions to repo

---
+++ laby2/zadA.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# Systemy operacyjne 2 – laboratorium nr 2
+#
+# Celem zajęć jest nabranie doświadczenia w podstawowej pracy z powłoką Bash,
+# w szczególności w nawigowaniu po drzewie katalogów i sprawdzaniu uprawnień.
+# Proszę unikać wykorzystywania narzędzia `find` w ramach bieżących zajęć.
+#
+# Nie przywiązujemy wagi do środowiska roboczego – zakładamy, że jego pliki,
+# inne niż te podane wprost w treści zadań, mogą ulec zmianie, a przygotowane
+# rozwiązania nadal powinny działać poprawnie (robić to, o czym zadanie mówi).
+#
+# Wszystkie chwyty dozwolone, ale ostatecznie w wyniku ma powstać tylko to,
+# o czym mowa w treści zadania – tworzone samodzielnie ewentualne tymczasowe
+# pliki pomocnicze należy usunąć.
+#
+
+#
+# Zadanie 10.
+# W katalogu `dane/deep/` i wszystkich jego podkatalogach odnaleźć wszystkie
+# pliki zwykłe, których nie możemy przeczytać ze względu na brak uprawnień
+# dostępu. Zwrócić ścieżki do odnalezionych plików względem katalogu `dane/`.
+# Opracowany skrypt powinien działać niezależnie od istniejącej liczby
+# i poziomów podkatalogów.
+#
+# Wskazówka: pomocne może być zdefiniowanie funkcji i jej rekurencje wywołanie.
+#
+cd dane/
+dir='deep'
+# ls -al $dir
+searcher()
+{
+    files=$(ls "${1}")
+    for file in $files
+    do 
+        if [[ ! -r "${1}/${file}" ]]; then
+            echo  "${1}/${file}"
+        elif [[ -d "${1}/${file}" ]]; then
+            searcher "${1}/${file}"
+        fi
+    done
+    #echo "$1"
+}
+searcher $dir
